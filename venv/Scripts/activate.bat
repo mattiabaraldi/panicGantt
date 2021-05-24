47beae1436428452a9d82cc4d31bf79eb58f06ad
mattiabaraldi <m.mattiabaraldi@gmail.com>
2021-05-24 18:38:40 +0200

    gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mbaraldi\Documents\Code\panicGantt\venv
+set VIRTUAL_ENV=C:\Users\drbar\Documents\Code\panicGantt\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
